9b894b851f0a06242b28c245b6fb34cb501423a1
＼ﾋｯｶﾘｰﾝ／ <s.nlf.fh@gmail.com>
2014-06-27 00:21:31 +0900

    update

--- test.cpp
+++ test.cpp
@@ -2,8 +2,8 @@
 // hikari
 
 #include <iostream>
-#include <vector>
 #include <string>
+#include <vector>
 
 #include <boost/version.hpp>
 #include <boost/format.hpp>
@@ -11,19 +11,12 @@
 using namespace std;
 using namespace boost;
 
-auto f()
-{
-	return 100'000'000;
-}
-
 int main(int argc, char *argv[], char *envp[])
 {
 	vector<string> argstr(argv, argv + argc);
 	vector<string> envstr(envp, [](char *i[]){while (*++i); return i;}(envp));
 
-	decltype(auto) y = f();
-	cout << format("はろー %1%\n\n") % y;
-
+	cout << format("はろー\n");
 	cout << format("Boost version : %1%.%2%.%3%\n\n") % (BOOST_VERSION / 100'000) % (BOOST_VERSION / 100 % 1'000) % (BOOST_VERSION % 100);
 
 	cout << format("コマンドライン引数\n");
@@ -39,3 +32,4 @@ int main(int argc, char *argv[], char *envp[])
 	cin >> argstr[0];
 	return 0;
 }
+
